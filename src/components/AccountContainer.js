864ded3ad60142e8f831c591cb4c320885183372
github username <github email address>
2018-09-05 11:03:23 -0400

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -11,19 +11,41 @@ class AccountContainer extends Component {
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
+    this.state = {
+      transactions: transactions,
+      show: transactions
+    }
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+    .this(res => res.json())
+    .this(json => this.setState({
+      transactions: json,
+      show: json
+    }))
+    // changed something last minute, working with ELbin, functionality of the app works, just fixing the fetch
+  }
+
+  handleChange = (event) => {
+    if (!event) {
+      this.setState({
+        ...this.state,
+        show: this.state.transactions
+      })
+    } else {
+      this.setState({
+        ...this.state,
+        show: this.state.transactions.filter(t => t.description.toLowerCase().includes(event) || t.category.toLowerCase().includes(event))
+      })
+    }
   }
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange}/>
+        <TransactionsList show={this.state.show} />
       </div>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Search = () => {
-  return (
-    <div className="ui huge fluid icon input">
-      <input
-        type="text"
-        placeholder={"Search your Recent Transactions"}
-      />
-      <i className="circular search link icon"></i>
-    </div>
-  )
+class Search extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value}, () => this.props.handleChange(this.state.value))
+  }
+
+  render() {
+    return (
+      <div className="ui huge fluid icon input">
+        <input
+          type="text" onChange={this.handleChange} value={this.state.value}
+          placeholder={"Search your Recent Transactions"}
+        />
+        <i className="circular search link icon"></i>
+      </div>
+    )
+  }
 }
 
 export default Search
