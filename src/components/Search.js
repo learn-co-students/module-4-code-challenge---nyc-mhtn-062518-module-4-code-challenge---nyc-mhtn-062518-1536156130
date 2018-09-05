ad517235f52909fbe5dcd6f5f1956b0b92f07f1d
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-09-05 12:17:17 -0400

    this is the right version

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-//I know I'm very close, my imput is controlled by state, but for some reason my filter isn't updating like I think it should. I went back and forth with setting this component to class-based, since the value changes when I type, but because handleChange is defined in the container I need to set state for the input there, if I want to access input within my handleChange function.
-
 const Search = (props) => {
   // console.log("props in Search", props)
   return (
